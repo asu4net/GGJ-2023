c4bfc7c794c0170d012de988d5cf516f50cc7e60
Alex <asu4net@gmail.com>
2023-02-04 12:02:22 +0100

    CarMovement prototype changes

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
     [SerializeField] private CarWaypoints waypointsSlot;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 10.0f;
 
-    // Update is called once per frame
+    private int currWaypoint = 0;
+    
+    void Awake()
+    {
+        if (!waypointsSlot)
+            waypointsSlot = FindObjectOfType<CarWaypoints>();
+        if (!waypointsSlot)
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            Debug.LogError("Waypoints missing");
+            #else
+            Application.Quit();
+            #endif
+        }
+    }
+    
     void Update()
     {
+        Vector3 destination = waypointsSlot.GetWaypointPos(currWaypoint);
+        Vector3 currentDir = (destination - transform.position).normalized;
+        transform.position += currentDir * (speed * Time.deltaTime);
         
+        if (Vector3.Distance(transform.position, destination) <= 0.1f)
+            currWaypoint++;
+        
+        if (waypointsSlot.Count() == currWaypoint)
+            currWaypoint = 0;
     }
 }
