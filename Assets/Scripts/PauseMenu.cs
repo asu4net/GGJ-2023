ccebe3f85ccb5487afdbacd0e2625b068b70f0e3
Alex <asu4net@gmail.com>
2023-02-04 20:40:33 +0100

    UICanvas

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenu : MonoBehaviour
+{
+    public UnityEvent<bool> OnPause;
+    
+    private Animator animator;
+    private bool gamePaused = false;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        SwitchPause();
+    }
+
+    public void OnButtonPlay()
+    {
+        SwitchPause();
+    }
+
+    public void OnButtonReset()
+    {
+        
+    }
+
+    public void OnButtonExit()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            Debug.LogError("Waypoints missing");
+#else
+        Application.Quit();
+#endif
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SwitchPause();
+    }
+
+    void SwitchPause()
+    {
+        animator.SetTrigger("Switch");
+        gamePaused = !gamePaused;
+        OnPause.Invoke(gamePaused);
+        Time.timeScale = gamePaused ? 0 : 1;
+    }
+}
